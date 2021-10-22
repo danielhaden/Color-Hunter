1311eac95a7654c5acb14ec2e3abdc96c8e442c6
Daniel Haden <haden.daniel@gmail.com>
2021-10-21 18:23:04 -0600

    finished Color Hunt scraper

--- ColorHunt.py
+++ ColorHunt.py
@@ -1,13 +1,151 @@
-from requests_html import HTMLSession
+import time
+import csv
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 class ColorHunt:
 
-    def get_html(self):
-        session = HTMLSession()
-        self.results = session.get(self.url)
+    filePath = "C:/Users/hadend.UNIVERSITY/Desktop/ColorHuntPalettes.csv"
+    mainURL = "https://colorhunt.co"
 
-    def __init__(self, url):
-        self.url = url
-        self.results = None
+    def __init__(self):
+        self.urls = []
+        self.collections = {}
+        self.browser = None
+
+    def initiate_browser(self):
+        """ Instantiate a Chrome browser driven by Selenium
+        :return: None
+        """
+        self.browser = webdriver.Chrome(
+            executable_path='C:/Users/hadend.UNIVERSITY/PycharmProjects\ColorPaletteWebScraper/chromedriver.exe')
+
+    def get_collections_urls(self):
+        """ Get the URLs of all Color Hunt collections (from menu bar on left side of page)
+        :return: None
+        """
+
+        # Start up Chrome browser
+        self.initiate_browser()
+
+        # Get source for Color Hunt's main page
+        self.browser.get(self.mainURL)
+        html = self.browser.page_source
+
+        # parse HTML and navigate to the relevant elements
+        soup = BeautifulSoup(html, 'html.parser')
+        body = soup.find("body")
+        tags = body.find("div", {"class": "tags"})
+        urls = tags.find_all("a", {"href": True})
+
+        # get the full URLs for Selenium and store them in object list
+        for url in urls:
+            self.urls.append(self.mainURL + url['href'])
+
+    def get_all_palettes_on_page(self, url):
+        """ Scrolls to bottom of page to load all palettes and scraps color
+        hex codes for each palette.
+        :param url: the URL of the collection/page
+        :return: None
+        """
+
+        # Go to the page
+        self.browser.get(url)
+
+        # Sets how rapidly the page is scrolled in seconds
+        pause_time_on_scroll = 0.2
+
+        # Get scroll height
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+
+        # Loop until we reach the bottom of the page
+        while True:
+            # Scroll down to bottom
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(pause_time_on_scroll)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+
+            # If the new scroll height is the same, we've reached the buttom of the page
+            if new_height == last_height:
+                break
+
+            # Otherwise, record the last scroll height before looping
+            last_height = new_height
+
+        # Get, parse, and scrape the page
+        html = self.browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        body = soup.find("body")
+        items = soup.find_all("div", {"class": "item"})
+
+        palettes = []
+        for item in items:
+            palette = item.find("div", {"class": "palette"})
+
+            c3 = palette.find("div", {"class": "place c3"})
+            c2 = palette.find("div", {"class": "place c2"})
+            c1 = palette.find("div", {"class": "place c1"})
+            c0 = palette.find("div", {"class": "place c0"})
+
+            colors = []
+            if c3.find("span", {"data-copy": True}) is not None:
+                colors.append(c3.find("span", {"data-copy": True})['data-copy'])
+
+            if c2.find("span", {"data-copy": True}) is not None:
+                colors.append(c2.find("span", {"data-copy": True})['data-copy'])
+
+            if c1.find("span", {"data-copy": True}) is not None:
+                colors.append(c1.find("span", {"data-copy": True})['data-copy'])
+
+            if c0.find("span", {"data-copy": True}) is not None:
+                colors.append(c0.find("span", {"data-copy": True})['data-copy'])
+
+            palettes.append(colors)
+
+        self.collections[url.rsplit('/', 1)[1]] = palettes
+
+    def get_all_palettes(self):
+        """Gets all palettes from every collection of Color Hunt"""
+
+        self.get_collections_urls()
+
+        for url in self.urls:
+            self.get_all_palettes_on_page(url)
+
+        self.browser.close()
+
+    def print_stats(self):
+        print("{} collections retrieved from Color Hunt.".format(len(self.collections)))
+
+        for key, collection in self.collections.items():
+            print("Color Hunt collection {}".format(key), "contains {} palettes.".format(len(collection)))
+
+    def save_to_csv(self):
+        """
+        Saves results to CSV file
+        :return: None
+        """
+
+        # Open the file (create if necessary)
+        with open(self.filePath, 'w+', newline='') as f:
+
+            # create the csv writer
+            writer = csv.writer(f)
+
+            for key, collection in self.collections.items():
+
+                for palette in collection:
+                    row = [key]
+
+                    row = row + palette
+
+                    if len(row) > 1:
+                        writer.writerow(row)
+
+            f.close()
 
--- main.py
+++ main.py
@@ -1,71 +1,12 @@
-import time
-
-from selenium import webdriver
-
-from bs4 import BeautifulSoup
-
 from ColorHunt import ColorHunt
 
 
 if __name__ == '__main__':
 
-    option = webdriver.ChromeOptions().add_argument(" - incognito")
-
-    browser = webdriver.Chrome(executable_path='C:/Users/hadend.UNIVERSITY/PycharmProjects\ColorPaletteWebScraper/chromedriver.exe',
-                               chrome_options=option)
-
-    browser.get("https://colorhunt.co/retro")
-
-    SCROLL_PAUSE_TIME = 0.5
-
-    # Get scroll height
-    last_height = browser.execute_script("return document.body.scrollHeight")
-
-    while True:
-        # Scroll down to bottom
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = browser.execute_script("return document.body.scrollHeight")
-
-        if new_height == last_height:
-            break
-        last_height = new_height
-
-    html = browser.page_source
+    ch = ColorHunt()
+    ch.get_all_palettes()
+    ch.print_stats()
+    ch.save_to_csv()
 
 
-    soup = BeautifulSoup(html, 'html.parser')
 
-    body = soup.find("body")
-
-    items = soup.find_all("div", {"class": "item"})
-
-    palettes = []
-    for item in items:
-        palette = item.find("div", {"class": "palette"})
-
-        c3 = palette.find("div", {"class": "place c3"})
-        c2 = palette.find("div", {"class": "place c2"})
-        c1 = palette.find("div", {"class": "place c1"})
-        c0 = palette.find("div", {"class": "place c0"})
-
-        colors = []
-        if c3.find("span", {"data-copy": True}) is not None:
-            colors.append(c3.find("span", {"data-copy": True})['data-copy'])
-
-        if c2.find("span", {"data-copy": True}) is not None:
-            colors.append(c2.find("span", {"data-copy": True})['data-copy'])
-
-        if c1.find("span", {"data-copy": True}) is not None:
-            colors.append(c1.find("span", {"data-copy": True})['data-copy'])
-
-        if c0.find("span", {"data-copy": True}) is not None:
-            colors.append(c0.find("span", {"data-copy": True})['data-copy'])
-
-        palettes.append(colors)
-
-    print(palettes)
